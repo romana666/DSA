d11c07b706d5c061e6862d1fcb4c3d1d032423d8
Romana Khanom <romanakhanom.02@gmail.com>
2026-08-03 21:51:46 +0600

    Add files via upload

---
+++ STL/Vector/01_declaration.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+int main(){
+
+    //vector <int> v;                   // empty vector
+    //vector <int> v(n);               //vector with fixed size
+    //vector <int> v(5);              //vector with fixed size
+    //vector <int> v(n,0);           //vector same with same value
+    //vector <int> v(5,15);         //vector with fixed saze ande same initialize value for example 15,15,15,15,15
+    //vector <int> v={1,2,3,4,5};  //initialize with values
+
+    //vector<int> v1 = {1,2,3,4,5}; 
+    //vector<int> v2(v1);                                   //copy another vector
+
+    //vector<int> v1 = {1,2,3,4,5}; 
+    //vector<int> v2 = (v1);                              // copy using assignment
+
+    //vector<int> v1 = {1,2,3,4,5}; 
+    //vector<int> v2(v1.begin(),v1.end());             //Create from a range (iterator)
+
+    //vector <vector<int>> matrix;                    // 2D vector
+    //vector<vector<int>> matrix(3, vector<int>(4)); //  2D vector with size
+
+
+    //vector<vector<vector<int>>> cube;            //3D vector
+    //vector<vector<vector<int>>> cube(
+    //2,
+    //vector<vector<int>>(3, vector<int>(4))     //3D vector with size
+//);
+
+    //vector<pair<int, int>> vp;                //vector of pairs
+
+    //vector <string> names;                   //vector of string
+
+    //vector <bool> visited(100,false);
+
+}
---
+++ STL/Vector/01_vector.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+int main(){
+    vector<int> v;
+
+    v.push_back(3);
+    v.push_back(2);
+    v.push_back(2);
+     v.push_back(9);
+
+
+    cout<<v[2]<<endl;
+    cout<<v.at(1)<<endl; 
+    //cout<<v.at(8)<<endl;       // by using at it tell that it is out of range
+    
+
+
+    //size() of vector
+    cout<<"size : "<<v.size()<<endl;
+
+
+
+
+
+    //vector size with loop
+    for(int i = 0; i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+
+
+
+    //1st value with front() 
+    cout<<"front value : "<<v.front()<<endl;
+
+
+
+    //last value with back()
+    cout<<"back value: "<<v.back()<<endl;
+
+    
+
+    //remove vector
+    //v.clear();
+    //cout<<v.size()<<endl;       //checking it cleared or not
+
+
+    //test the vector is it blank
+    if(v.empty()){
+        cout<<"Empty"<<endl;
+    }
+    else{
+        cout<<"Not Empty"<<endl;
+    }
+
+
+    //delete the last value
+    v.pop_back();
+
+     for(int i = 0; i<v.size();i++){
+
+        cout<<v[i]<<" ";
+        
+    }
+    cout<<endl;
+
+    return 0 ;
+}
